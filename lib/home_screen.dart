c50419a176606c745a78ddef98ffa83cd6e1504a
Jirachai Thiemsert <cray.j.thiemsert@gmail.com>
2020-08-07 18:22:21 +0700

    Initial commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ft_bloc_firebase_login/authentication_bloc/authentication_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String name;
+
+  HomeScreen({Key key, @required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context).add(AuthenticationLoggedOut());
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Text('Welcome $name!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ft_bloc_firebase_login/authentication_bloc/authentication_bloc.dart';
+import 'package:ft_bloc_firebase_login/login/login_screen.dart';
+import 'package:ft_bloc_firebase_login/simple_bloc_observer.dart';
+import 'package:ft_bloc_firebase_login/splash_screen.dart';
+import 'package:ft_bloc_firebase_login/user_repository.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final UserRepository userRepository = UserRepository();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(userRepository: userRepository)
+        ..add(AuthenticationStarted()),
+      child: App(userRepository: userRepository),
+    )
+  );
+}
+
+class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if(state is AuthenticationInitial) {
+            return SplashScreen();
+          }
+          if(state is AuthenticationFailure) {
+            return LoginScreen(userRepository: _userRepository);
+          }
+          if(state is AuthenticationSuccess) {
+            return HomeScreen(name: state.displayName);
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
+
+
