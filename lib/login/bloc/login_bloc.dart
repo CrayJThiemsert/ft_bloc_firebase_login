badaa91bb7badb3fe6a65c6044bb0642cd4a0237
Jirachai Thiemsert <cray.j.thiemsert@gmail.com>
2020-08-09 04:29:14 +0700

    - Fixed: Able to google signed in but not not go to home screen. We have to reopen app. - Cause: Missing "yield LoginState.success();" after google signed success.

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -70,6 +70,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   Stream<LoginState> _mapLoginWithGooglePressedToState() async* {
     try {
       await _userRepository.signInWithGoogle();
+      yield LoginState.success();
     } catch(_) {
       yield LoginState.failure();
     }
